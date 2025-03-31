a6389a9253288c465064208612fc9647a21a0e3e
user <toome4070@gmail.com>
2025-03-31 08:57:11 +0200

    добавлены исправления

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
+
   $('.call-button').on('click', function() {
       $('#overlay').removeClass('hidden');
       $('#modal').removeClass('hidden');
+      $('.page').addClass('no-scroll');
+
+      let img = $('.header__menu').find('img');
+      img.attr('src', './assets/images/menu.svg');
+      $('.mobile-nav').removeClass('active');
   });
 
   $('#modal-close').on('click', function() {
       $('#overlay').addClass('hidden');
       $('#modal').addClass('hidden');
+      $('.page').removeClass('no-scroll');
   });
 
   $('#overlay').on('click', function() {
@@ -16,12 +23,21 @@ $(document).ready(function() {
 
   $('.header__menu').click(function() {
     let img = $(this).find('img');
+
     if (img.attr('src') === './assets/images/menu.svg') {
         img.attr('src', './assets/images/close-menu.svg');
-        $('.mobile-nav').addClass('active'); 
+        $('.mobile-nav').addClass('active');
+        $('#overlay').removeClass('hidden');
+        $('.page').addClass('no-scroll');
     } else {
         img.attr('src', './assets/images/menu.svg');
-        $('.mobile-nav').removeClass('active'); 
+        $('.mobile-nav').removeClass('active');
+        $('#overlay').addClass('hidden');
+        $('.page').removeClass('no-scroll');
     }
+
+  });
+
 });
-});
+
+
