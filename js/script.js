e7be3c5200726bac607aafd80ef334417a789945
user <toome4070@gmail.com>
2025-04-01 10:10:14 +0200

    добавлены аккордион и карта

--- js/script.js
+++ js/script.js
@@ -54,3 +54,21 @@ $(document).ready(function() {
 
 });
 
+
+$('.faq__list').accordion({
+  active: true,
+  collapsible: true,
+  heightStyle: 'content',
+});
+
+
+const map = L.map('map').setView([55.755819, 37.617644], 10);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([55.755819, 37.617644]).addTo(map)
+    .bindPopup('55.755819, 37.617644')
+    .openPopup();
+
